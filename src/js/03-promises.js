95edae8ffbc3caf372b2bf3815a0a94dfebcf059
Nedzhat <nexuak@gmail.com>
2022-06-17 21:29:23 +0300

    added third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formRef = document.querySelector('.form');
+formRef.addEventListener('submit', startCreatingPromise);
+
+let delay;
+let step;
+let amount;
+
+function startCreatingPromise(e) {
+  e.preventDefault();
+  delay = Number(e.target.elements.delay.value);
+  step = Number(e.target.elements.step.value);
+  amount = Number(e.target.elements.amount.value);
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(value => {
+        Notify.success(value);
+      })
+      .catch(error => {
+        Notify.failure(error);
+      });
+    delay += step;
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
 }
